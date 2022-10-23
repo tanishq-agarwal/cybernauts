2c004ee03bb307b82e6622f68bca642439907e96
tanishq-agarwal <agarwaltanishq35@gmail.com>
2022-10-23 22:07:50 +0530

    deploying to heroku

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/productRegistration", {useNewUrlParser: true, useUnifiedTopology: true})
+const DB = 'mongodb+srv://tanishq:agarwal@cluster0.jjljg1w.mongodb.net/mernstack?retryWrites=true&w=majority'
+
+mongoose.connect(DB, {useNewUrlParser: true, useUnifiedTopology: true})
 .then( () => console.log("connection successfull..."))
 .catch((err) => console.log(err));
+
+// mongoose.connect("mongodb://localhost:27017/productRegistration", {useNewUrlParser: true, useUnifiedTopology: true})
+// .then( () => console.log("connection successfull..."))
+// .catch((err) => console.log(err));
